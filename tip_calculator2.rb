55a0001249968f0270344c5c0041661149827b5b
Jaina Morgan <jaina.morgan@gmail.com>
2014-01-16 17:46:14 -0500

    tip calculator w/ user input

---
+++ tip_calculator2.rb
@@ -0,0 +1,18 @@
+print "Meal cost: "
+meal = Float(gets.chomp)
+
+print "Tax percent (non-decimal): "
+tax = Integer(gets.chomp)
+
+print "Tip percent (non-decimal): "
+tip = Integer(gets.chomp)
+
+tax_value = meal * tax / 100
+meal_with_tax = meal + tax_value
+tip_value = meal_with_tax * tip / 100
+total_cost = meal_with_tax + tip_value
+
+puts "The pre-tax cost of your meal was $%.2f." % meal
+puts "At %d%%, tax for this meal is $%.2f." % [tax, tax_value]
+puts "For a %d%% tip, you should leave $%.2f." % [tip, tip_value]
+puts "The grand total for this meal is then $%.2f." % total_cost
